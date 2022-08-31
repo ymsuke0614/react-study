b6efb4c126afdc9f344f3431f9b40aaed19a2d2a
dyamakaw-git <yamasuke1725@gmail.com>
2022-08-31 11:51:14 +0900

    feat: color rating

--- src/ColorList.js
+++ src/ColorList.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({
+    colors = [],
+    onRemoveColor = f => f,
+    onRateColor = f => f
+ }) {
   if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
-
   return (
     <div>
       {colors.map(color => (
-        <Color key={color.id} {...color} />
+        <Color
+          key={color.id}
+          {...color}
+          onRemove={onRemoveColor}
+          onRate={onRateColor}
+        />
       ))}
     </div>
   );
--- src/StarRating.js
+++ src/StarRating.js
@@ -3,11 +3,19 @@ import Star from "./Star";
 
 const createArray = length => [...Array(length)];
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating({
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = f => f
+}) {
   return (
     <>
       {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => onRate(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
