39d2a1078f28f8e3713777117c74504b592136bf
dyamakaw-git <yamasuke1725@gmail.com>
2022-09-01 11:06:58 +0900

    refactor: use context provider and custom hooks

--- src/ColorList.js
+++ src/ColorList.js
@@ -1,22 +1,15 @@
 import React from "react";
 import Color from "./Color";
+import { useColors } from "./ColorProvider";
 
-export default function ColorList({
-    colors = [],
-    onRemoveColor = f => f,
-    onRateColor = f => f
- }) {
+export default function ColorList() {
+  const { colors } = useColors();
+
   if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
+
   return (
     <div>
-      {colors.map(color => (
-        <Color
-          key={color.id}
-          {...color}
-          onRemove={onRemoveColor}
-          onRate={onRateColor}
-        />
-      ))}
+      {colors.map(color => <Color key={color.id} {...color} />)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ColorProvider from "./ColorProvider";
+import { render } from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+render(
+  <ColorProvider >
     <App />
-  </React.StrictMode>
-);
+  </ColorProvider>,
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
